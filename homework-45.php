d54e7ce7e3da2f5b388264ad19b2f8b93a778226
hyunyoung1205 <127705794+hyunyoung1205@users.noreply.github.com>
2023-04-17 11:18:50 +0900

    Update homework-45.php

--- homework-45.php
+++ homework-45.php
@@ -68,17 +68,17 @@
 
     <h2>5번 문제</h2>
     <?php
-    if((isset($_POST['y']) && strlen($_POST['y']) > 0) && (isset($_POST['m']) && strlen($_POST['m']))){
+    if(isset($_POST['y']) && strlen($_POST['y']) > 0 && isset($_POST['m']) && strlen($_POST['m']) > 0){
     $m = $_POST["m"];
     $y = $_POST["y"];
     $wday=1;
         if(checkdate($m,1,$y)) {
             $firstweekday = getDate(mktime(0,0,0,$m,1,$y)); //해당 월 1일의 요일
-            $firstweekday = $firstweekday[$wday];
+            $firstweekday = $firstweekday['wday'];
             $lastday = date("t", mktime(0,0,0,$m,1,$y)); //t = 주어진 월의 총 일 수(ex : 2014년 1월 = "31" 일)
             $fc = ceil(($firstweekday+$lastday)/7);
             $count = $fc * 7; 
-            $j=0;
+            $j=1;
 
         echo "<table border='1' width=\"500\" bordercolor=\"#0000FF\">";
         echo "<tr align=\"center\" bgcolor=\"#66FFFF\"><td colspan=\"7\">". $y."년 ".$m."월 달력</td></tr>";
@@ -89,8 +89,8 @@
                 echo "<tr>";
             }
             echo "<td>";
-                if($i>$firstweekday['wday'] && $j < $lastday){
-                    echo $j + 1;
+                if($i>$firstweekday && $j <= $lastday){
+                    echo $j;
                     $j++;
                 }else{
                     echo "&nbsp;";
@@ -108,6 +108,5 @@
         }
     }
 ?>
-
     </body>
 </html>
